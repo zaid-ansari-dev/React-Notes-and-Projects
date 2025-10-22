e25b8926ccfa888961c36479341913e6660ead18
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-10-22 06:36:32 +0530

    Split Header into a Component and use Link

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,39 +1,13 @@
-import "./header.css";
+import "../components/header.css";
 import "./tracking.css";
+import { Header } from '../components/Header';
 
 export function TrackingPage() {
   return (
     <>
       <title>Tracking</title>
 
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
+      <Header/>
 
       <div className="tracking-page">
         <div className="order-tracking">
