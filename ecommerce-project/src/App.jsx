0522204939efdd0589f814490a2bfbfe0da91f18
zaid-ansari-dev <zaid.ansari.dev@proton.me>
2025-10-22 05:48:29 +0530

    Moved Orders & Tracking Pages to React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router';
 import './App.css'
-import {HomePage} from './pages/HomePage.jsx'
-import {CheckoutPage} from './pages/CheckoutPage.jsx';
+import { HomePage } from './pages/HomePage.jsx'
+import { CheckoutPage } from './pages/CheckoutPage.jsx';
+import { OrdersPage } from './pages/OrdersPage.jsx';
+import { TrackingPage } from './pages/TrackingPage.jsx';
 console.log('hello')
 function App() {
 
   return ( //instead of path='/' u could just use index 
     <Routes>
-      <Route path='/' element={<HomePage/>}/> 
+      <Route index element={<HomePage/>}/> 
       <Route path='checkout' element={<CheckoutPage/>}/>
+      <Route path='orders' element={<OrdersPage/>}/>
+      <Route path='tracking' element={<TrackingPage/>}/>
     </Routes>
   )
 }
